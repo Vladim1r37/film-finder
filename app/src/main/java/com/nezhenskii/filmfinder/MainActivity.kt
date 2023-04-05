3d2dd570a3f650589965972e29cb8e56d300b935
Vladim1r37 <vl4d1m1r37@gmail.com>
2023-04-05 23:12:39 +0300

    interface edited

--- app/src/main/java/com/nezhenskii/filmfinder/MainActivity.kt
+++ app/src/main/java/com/nezhenskii/filmfinder/MainActivity.kt
@@ -2,10 +2,31 @@ package com.nezhenskii.filmfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initMenuButtons()
+    }
+
+    private fun initMenuButtons() {
+        button_menu.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        button_favourites.setOnClickListener {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        button_watch_later.setOnClickListener {
+            Toast.makeText(this, "Смотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        button_selections.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        button_settings.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
