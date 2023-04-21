215d1af3eeecfc86bc7a1703f8890ae3a736278d
Vladim1r37 <vl4d1m1r37@gmail.com>
2023-04-21 23:28:40 +0300

    listeners to menu buttons added

--- app/src/main/java/com/nezhenskii/filmfinder/MainActivity.kt
+++ app/src/main/java/com/nezhenskii/filmfinder/MainActivity.kt
@@ -9,24 +9,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initMenuButtons()
+        initNavigation()
     }
 
-    private fun initMenuButtons() {
-        button_menu.setOnClickListener {
-            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+    private fun initNavigation() {
+        topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.settings -> {
+                    Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
-        button_favourites.setOnClickListener {
-            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
-        }
-        button_watch_later.setOnClickListener {
-            Toast.makeText(this, "Смотреть позже", Toast.LENGTH_SHORT).show()
-        }
-        button_selections.setOnClickListener {
-            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
-        }
-        button_settings.setOnClickListener {
-            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        bottom_navigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть похже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
