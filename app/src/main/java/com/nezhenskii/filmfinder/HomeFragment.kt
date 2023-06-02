f0fc02c6bc5683e6b5f7b6a263ce124835609d5f
Vladim1r37 <vl4d1m1r37@gmail.com>
2023-06-02 11:41:25 +0300

    Search View added

--- app/src/main/java/com/nezhenskii/filmfinder/HomeFragment.kt
+++ app/src/main/java/com/nezhenskii/filmfinder/HomeFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
 
 
 class HomeFragment : Fragment() {
@@ -29,6 +31,26 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRecyclerView()
+        search_view.setOnClickListener {
+            search_view.isIconified = false
+        }
+        search_view.setOnQueryTextListener (object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                if (newText.isEmpty()) {
+                    filmsAdapter.addItems((activity as MainActivity).getData())
+                    return true
+                }
+                val result = (activity as MainActivity).getData().filter {
+                    it.title.lowercase(Locale.getDefault()).contains(newText.lowercase(Locale.getDefault()))
+                }
+                filmsAdapter.addItems(result)
+                return true
+            }
+        })
     }
 
     private fun initRecyclerView() {
